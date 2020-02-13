cc14a1c2f38b126b521ee22ec6ddf6a5359e9ae4
Dragon1573 <Legend_1949@126.com>
2020-02-13 14:01:53 +0800

    更新 utils.Configurations
    1. 增加2个成员属性并生成相应的 Getter 和 Setter
    2. 完善所有成员方法的 JavaDoc

--- homework/src/main/java/utils/Configurations.java
+++ homework/src/main/java/utils/Configurations.java
@@ -1,5 +1,8 @@
 package utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 用户配置实例类
  *
@@ -9,6 +12,8 @@ public class Configurations {
     private String email = "";
     private String smtp = "";
     private String password = "";
+    private String target = "";
+    private List<String> targetList = new ArrayList<>();
 
     /**
      * 设置配置
@@ -28,6 +33,12 @@ public class Configurations {
         }
     }
 
+    /**
+     * 输出相应的配置
+     *
+     * @param item
+     *     配置键
+     */
     public void show(final String item) {
         if (Constants.EMAIL.equals(item)) {
             System.out.println(getEmail());
@@ -40,33 +51,57 @@ public class Configurations {
 
     /**
      * 发件电子邮箱
+     *
+     * @return {@link String}
      */
     public final String getEmail() {
         return email;
     }
 
+    /**
+     * 设置发件邮箱地址
+     *
+     * @param email
+     *     电子邮箱地址
+     */
     public void setEmail(final String email) {
         this.email = email;
     }
 
     /**
      * 发件服务器地址
+     *
+     * @return {@link String}
      */
     public final String getSmtp() {
         return smtp;
     }
 
+    /**
+     * 设置发件服务器地址
+     *
+     * @param smtp
+     *     发件SMTP服务器地址
+     */
     public void setSmtp(final String smtp) {
         this.smtp = smtp;
     }
 
     /**
      * 邮箱登陆密码
+     *
+     * @return {@link String}
      */
     public final String getPassword() {
         return password;
     }
 
+    /**
+     * 设置发件密码/授权码
+     *
+     * @param password
+     *     密码/授权码
+     */
     public void setPassword(final String password) {
         this.password = password;
     }
@@ -80,5 +115,42 @@ public class Configurations {
         return (
             "".equals(getEmail()) && "".equals(getSmtp()) && "".equals(getPassword())
         );
+    }
+
+    /**
+     * 获取默认收件地址
+     *
+     * @return {@link String}
+     */
+    public String getTarget() {
+        return target;
+    }
+
+    /**
+     * 设置默认收件人
+     *
+     * @param target
+     *     默认收件人地址
+     */
+    public void setTarget(final String target) {
+        this.target = target;
+    }
+
+    /**
+     * 获取可选收件人列表
+     *
+     * @return {@link List}
+     */
+    public List<String> getTargetList() {
+        return targetList;
+    }
+
+    /**
+     * 设置可选收件人列表
+     *
+     * @param targetList 收件人列表
+     */
+    public void setTargetList(final List<String> targetList) {
+        this.targetList = targetList;
     }
 }
